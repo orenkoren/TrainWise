685fe6bcf1d0fc993fb04d065973db856c053feb
Oren <outbreaker99@gmail.com>
2018-09-26 23:02:34 +0300

    Initial Commit

---
+++ TrainWise/TrainWise/TrainWise/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TrainWise.Models;
+
+namespace TrainWise.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
